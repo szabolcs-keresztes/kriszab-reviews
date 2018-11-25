c90819023dc92f0afc3d907adcf53da71e9cba31
szabi.keresztes <szabi.keresztes@bjss.com>
2018-11-25 18:11:52 +0000

    getting the page together

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar, faDollarSign } from '@fortawesome/free-solid-svg-icons'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>Kriszab Reviews</h1>
+        <h2>Bill's Restaurant</h2>
+        <img src="assets/IMG_20181118_155527.jpg"></img>
+        <p>The review goes here</p>
+        <div>
+          <div>
+            Hospitality - 
+            <FontAwesomeIcon icon={faStar}/>
+            <FontAwesomeIcon icon={faStar}/>
+          </div>
+          <div>
+            Food / drink - 
+            <FontAwesomeIcon icon={faStar}/>
+            <FontAwesomeIcon icon={faStar}/>
+            <FontAwesomeIcon icon={faStar}/>
+            <FontAwesomeIcon icon={faStar}/>
+          </div>
+          <div>
+            Place - 
+            <FontAwesomeIcon icon={faStar}/>
+            <FontAwesomeIcon icon={faStar}/>
+            <FontAwesomeIcon icon={faStar}/>
+            <FontAwesomeIcon icon={faStar}/>  
+          </div>
+          <div>
+            Price -
+            <FontAwesomeIcon icon={faDollarSign}/>
+            <FontAwesomeIcon icon={faDollarSign}/>
+            <FontAwesomeIcon icon={faDollarSign}/>
+          </div>
+        </div>
       </div>
     );
   }
